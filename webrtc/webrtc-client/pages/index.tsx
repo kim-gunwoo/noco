0a724a21a3c1f103cb90b38acbfa69b431fcc0d6
git-k <kgunu93@gmail.com>
2022-11-08 21:54:44 +0900

    feat: init add

--- webrtc/webrtc-client/pages/index.tsx
+++ webrtc/webrtc-client/pages/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next";
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
+import { io } from "socket.io-client";
+
+const configuration = {
+  iceServers: [
+    {
+      urls: ["stun:tk-turn2.xirsys.com"],
+    },
+    {
+      usernames: "DUCG",
+      credential: "",
+      urls: [],
+    },
+  ],
+};
 
 const Home: NextPage = () => {
   const localVideoRef = useRef<HTMLVideoElement>(null);
+  const roomIdRef = useRef("randomString");
+  const pcRef = useRef(new RTCPeerConnection(configuration));
+  const socketRef = useRef(null);
 
   const getLocalVideo = async () => {
     const mediaStream = await navigator.mediaDevices.getUserMedia({
@@ -13,6 +30,11 @@ const Home: NextPage = () => {
     if (!localVideoRef.current) return;
     localVideoRef.current.srcObject = mediaStream;
     // localVideoRef.current.play();
+  };
+
+  const handleJoinBtn = () => {
+    const socket = io("ws://localhost:8000");
+    socketRef.current = socket;
   };
 
   useEffect(() => {
@@ -27,6 +49,7 @@ const Home: NextPage = () => {
     <div>
       <LocalVideo ref={localVideoRef} muted />
       <button onClick={getLocalVideo}>my camera</button>
+      <button onClick={getLocalVideo}></button>
     </div>
   );
 };
