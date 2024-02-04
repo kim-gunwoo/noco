a1262777c3302a61da6139ddf205b1e5620eb97c
git-k <kgunu93@gmail.com>
2024-02-04 18:06:34 +0900

    feat: add metadata

--- learn-nextjs14/components/Navigation.tsx
+++ learn-nextjs14/components/Navigation.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Naviation() {
+export default function Navigation() {
   const path = usePathname();
 
   return (
