ae25380152b399376a5a12a071add7519f89a1cb
git-k <kgunu93@gmail.com>
2024-02-06 21:47:57 +0900

    feat: add fetch data

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -2,6 +2,15 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Home() {
-  return <div>home</div>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
